cba7fa0a7aba6d1a2d3a976eca1d7c05315637bc
Josh Waihi <josh.wahi@acquia.com>
2016-07-12 15:05:10 +1200

    Added command to list entities

---
+++ src/Command/EntitiesListCommand.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\Table;
+use Config\ClientConfig;
+
+class EntitiesListCommand extends Command
+{
+  protected function configure()
+    {
+        $this
+            ->setName('entities:list')
+            ->setDescription('List entities')
+            ->addOption(
+               'config',
+               'c',
+               InputOption::VALUE_OPTIONAL,
+               'Specify the config file to load api client credentials out of.',
+               getenv('HOME') . '/.content-hub-client-cli-config'
+            )
+            ->addOption(
+               'limit',
+               'l',
+               InputOption::VALUE_OPTIONAL,
+               'Limit the number of results',
+               1000
+            )
+            ->addOption(
+               'start',
+               'o',
+               InputOption::VALUE_OPTIONAL,
+               'Offset to start from.',
+               0
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $config = ClientConfig::loadFromInput($input);
+
+        $options = [
+          'limit' => $input->getOption('limit'),
+          'start' => $input->getOption('start'),
+          'fields' => 'title'
+        ];
+
+        $client = $config->loadClient();
+        $entities = $client->listEntities(array_filter($options));
+
+        $rows = [];
+        foreach ($entities['data'] as $entity) {
+          $row = $entity;
+          unset($row['attributes']);
+          if (!empty($entity['attributes']['title'])) {
+            $row['title'] = array_shift($entity['attributes']['title']);
+          }
+          $rows[] = $row;
+        }
+
+        $table = new Table($output);
+        $table
+            ->setHeaders(array('UUID', 'Origin', 'Modified', 'Type', 'Title'))
+            ->setRows($rows)
+        ;
+        $table->render();
+    }
+
+}
